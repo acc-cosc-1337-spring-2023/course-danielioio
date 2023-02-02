7f50c5eefe0432215ff42eb3752a07231c9a2a2a
danielioio <123427710+danielioio@users.noreply.github.com>
2023-02-02 02:17:51 +0000

    unit test completed, main in progress

--- src/homework/01_data_types/main.cpp
+++ src/homework/01_data_types/main.cpp
@@ -1,15 +1,19 @@
 ﻿//write include statements
+#include<iostream>
+#include "data_types.h"
 
 
 //write namespace using statement for cout
-
+using std::cout;
 
 /*
 Call multiply_numbers with 10 and 10 parameter values and display function result
 */
 int main()
 {
-	
+	int num = 0;
+	cin<<"Enter a number: "<<num<<"\n";
+	int result = multiply_numbers(num)
 
 	return 0;
 }
--- test/examples/01_data_types/01_data_types_tests.cpp
+++ test/examples/01_data_types/01_data_types_tests.cpp
@@ -1,10 +1,13 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
-
+#include "data_types.h"
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
-
+TEST_CASE("Test Multiply Numbers", "validate multiplication") {
+	REQUIRE(multiply_numbers(10) == 50);
+	REQUIRE(multiply_numbers(2) == 10);
+}
 /*test case echo_variable */
 
 /*test case add_to_double_1 with 0 as parameter*/
