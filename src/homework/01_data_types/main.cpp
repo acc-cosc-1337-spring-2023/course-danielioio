93a38bd49fd1d0f979cd79320b5f097d28df8ef0
danielioio <123427710+danielioio@users.noreply.github.com>
2023-02-04 21:12:33 +0000

    homework 1 with unit testing

--- src/homework/01_data_types/main.cpp
+++ src/homework/01_data_types/main.cpp
@@ -4,16 +4,19 @@
 
 
 //write namespace using statement for cout
-using std::cout;
+
 
 /*
 Call multiply_numbers with 10 and 10 parameter values and display function result
 */
 int main()
 {
-	int num = 0;
-	cin<<"Enter a number: "<<num<<"\n";
-	int result = multiply_numbers(num)
-
+	int num= 0;
+	std::cin>>num;
+	int result = multiply_numbers(num);
+	std::cout<<result<<"\n";
+	int num1 = 4;
+	multiply_numbers(num1);
+	std::cout<<result<<"\n";
 	return 0;
-}
+	}
